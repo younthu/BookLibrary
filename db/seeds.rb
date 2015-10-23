51cada2183a741edadc7c01a7672b747dd695d72
Andrew(Zhiyong) Yang <evil_eve_live@outlook.com>
2015-10-23 09:42:22 +0800

    Create admin in seeds.rb use 'rake db:seed' to create admin account

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+user = User.create(:email => "admin@admin.com", :password => "11111111", :password_confirmation=>"11111111", :admin=>true)
+
+p user.errors
